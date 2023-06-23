d7f85bfc40248b691300fa33e60f6e9077db522f
Anurag Nagar <nagaranurag1999@gmail.com>
2023-06-23 23:14:16 +0530

    Improve performance fix

--- accounts/__init__.py
+++ accounts/__init__.py
@@ -1,46 +1,67 @@
 import os
 from flask import Flask
-from accounts.extentions import (
-        login_manager,
-        bootstrap,
-        database,
-        csrf
-    )
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 def create_app():
-
+    """
+    Create and configure the Flask application instance.
+    """
     app = Flask(__name__, template_folder="templates")
 
-    app.config['DEBUG'] = True
-    app.config['TESTING'] = True
-    app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", None)
-
-    # SQL configuration
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-    app.config["SQLALCHEMY_ECHO"] = False
-
+    # application configuration.
+    config_application(app)
     # register account blueprint.
-    config_blueprint(app=app)
+    config_blueprint(app)
     # config application extension. 
-    config_app(app=app)
+    config_extention(app)
 
     return app
 
-def config_app(app):
-    from .modals import User
+def config_application(app):
+    app.config["DEBUG"] = True
+    app.config["TESTING"] = True
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", None)
+
+    # SQLAlchemy configuration
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+def config_blueprint(app):
+    """
+    Configure and register blueprints with the Flask application.
+    """
+    from .views import accounts
+    app.register_blueprint(accounts)
+
+def config_extention(app):
+    """
+    Configure application extensions.
+    """
+    from .extentions import login_manager
+    from .extentions import bootstrap
+    from .extentions import database
+    from .extentions import migrate
+    from .extentions import csrf
+    
     login_manager.init_app(app)
     bootstrap.init_app(app)
     database.init_app(app)
+    migrate.init_app(app, db=database)
     csrf.init_app(app)
+    config_manager(login_manager)
 
-    @login_manager.user_loader
+def config_manager(manager):
+    """
+    Configure with Flask-Login manager.
+    """
+    from .modals import User
+
+    manager.login_message = "You are not logged in to your account."
+    manager.login_message_category = "warning"
+    manager.login_view = "accounts.login"
+
+    @manager.user_loader
     def user_loader(id):
         return User.query.get_or_404(id)
-
-def config_blueprint(app):
-    from .views import accounts
-    app.register_blueprint(accounts)
