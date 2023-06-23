26f2120c554c5316064a71d472784036cad4999d
Anurag Nagar <nagaranurag1999@gmail.com>
2023-06-23 23:24:12 +0530

    Improvement in app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from accounts import create_app, database
+from accounts import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    with app.app_context():
-        database.create_all()
     app.run()
