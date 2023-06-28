6c42e853b803b19846a42983115655ceab68cb82
Anurag Nagar <nagaranurag1999@gmail.com>
2023-06-28 23:15:22 +0530

    Added Mail() extention from flask-mail

--- accounts/extentions.py
+++ accounts/extentions.py
@@ -2,6 +2,7 @@ from flask_bootstrap import Bootstrap5
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
+from flask_mail import Mail
 from flask_migrate import Migrate
 
 # A bootstrap5 class for styling client side. 
@@ -15,6 +16,9 @@ database = SQLAlchemy()
 
 # login manager for managing user authentication.
 login_manager = LoginManager()
+
+# flask-mail for sending email.
+mail = Mail()
 
 # flask_migrate - Migration for database
 migrate = Migrate(command='db') 
