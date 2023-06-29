0674aa3bf03beac7ebd006a666f02ed29a6ba9e5
Anurag Nagar <nagaranurag1999@gmail.com>
2023-06-29 23:43:16 +0530

    Added send_mail send_reset_password send_reset_email method.

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,8 +1,40 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
+from flask_mail import Message
+from accounts.extensions import mail
 import uuid
+import os
+
 
 def unique_uid():
     return str(uuid.uuid4())
 
 def page_not_found():
     return render_template('error.html')
+
+def send_mail(subject, recipients, body):
+    sender = os.environ.get('MAIL_USERNAME', None)
+    message = Message(
+            subject=subject, sender=sender, recipients=[recipients]
+        )
+    message.body = body
+    print(message.body)
+    mail.connect()
+    mail.send(message)
+
+def send_reset_password(email=None):
+
+    subject = "Resest Your Password."
+    recipient = email
+
+    reset_link = url_for('accounts.reset_password')
+    content = f"Please click the following link to reset your password:\n {reset_link}"
+    send_mail(subject=subject, recipients=recipient, body=content)
+
+def send_reset_email(email=None):
+
+    subject = "Please Confirm Your Email."
+    recipient = email
+
+    confirmation_link = ""
+    content = f"Please click the following link to confirm your email:\n {confirmation_link}"
+    send_mail(subject=subject, recipients=recipient, body=content)
