65086f931f001a6a5e711a1e38a5945e54b32591
Anurag Nagar <nagaranurag1999@gmail.com>
2023-07-02 21:28:29 +0530

    Removing Activation Modal

--- accounts/modals.py
+++ accounts/modals.py
@@ -1,10 +1,15 @@
+from flask import url_for
 from flask_login.mixins import UserMixin
 from werkzeug.security import (
         generate_password_hash,
         check_password_hash
     )
 from accounts.extensions import database as db
-from accounts.utils import unique_uid, send_mail
+from accounts.utils import (
+        unique_uid,
+        unique_security_token,
+        send_mail
+    )
 from datetime import datetime
 
 
@@ -19,10 +24,12 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
 
+    active = db.Column(db.Boolean, default=False, nullable=False)
+    security_token = db.Column(db.String(138), default=unique_security_token, nullable=False)
+
     created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now, nullable=False)
 
-    account = db.Relationship('Activation', backref='user', cascade='save-update, merge, delete')
     profile = db.Relationship('Profile', backref='user', cascade='save-update, merge, delete')
 
 
@@ -31,12 +38,12 @@ class User(db.Model, UserMixin):
         A method for sending an email for account confirmation.
         """
         subject = "Verify Your Account."
-        verification_link = ""
+        verification_link = url_for('accounts.confirm_account', token=self.security_token)
         content = f"""
         Hi, {self.username}
         Your Registration is completed. 
 
-        Please click the following link to verify your account.
+        Please click the following link to confirm your account.
         {verification_link}
         """
 
@@ -59,41 +66,14 @@ class User(db.Model, UserMixin):
     def save_profile(self):
         profile = Profile(user_id=self.id)
         profile.save()
-    
-    def save_activation(self):
-        active = Activation(user_id=self.id)
-        active.save()
 
     def save(self):
         db.session.add(self)
         db.session.commit()
-        self.save_activation()
         self.save_profile()
         
     def __repr__(self):
         return '<User> {}'.format(self.email)
-
-
-class Activation(db.Model):
-
-    __tablename__ = 'activation'
-
-    id = db.Column(db.String(38), primary_key=True, default=unique_uid, unique=True, nullable=False)
-    active = db.Column(db.Boolean, default=False, nullable=False)
-    security_code = db.Column(db.String(6), default='', nullable=False)
-    security_token = db.Column(db.Boolean, default=True, nullable=False)
-
-    created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
-    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now, nullable=False)
-
-    user_id = db.Column(db.String(38), db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
-
-    def __repr__(self):
-        return '<Activation> {}'.format(self.user.username)
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
 
 
 class Profile(db.Model):
