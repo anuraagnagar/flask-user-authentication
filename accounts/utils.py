62c96725ed4c10c3a481b19bf78c9d564d954c48
Anurag Nagar <nagaranurag1999@gmail.com>
2023-07-02 22:25:19 +0530

    Added new generate security token method

--- accounts/utils.py
+++ accounts/utils.py
@@ -2,17 +2,21 @@ from flask import render_template, redirect, url_for
 from flask_mail import Message
 from accounts.extensions import mail
 import uuid
+import secrets
 import os
-
 
 def unique_uid():
     return str(uuid.uuid4())
+
+def unique_security_token(token_len=48):
+    return secrets.token_hex(token_len)
 
 def page_not_found():
     return render_template('error.html')
 
 def send_mail(subject, recipients, body):
     sender = os.environ.get('MAIL_USERNAME', None)
+    print(sender)
     message = Message(
             subject=subject, sender=sender, recipients=[recipients]
         )
