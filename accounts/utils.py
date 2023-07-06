9fe2aac8976fdbf246f5863b5d6ed579cdf1112e
Anurag Nagar <nagaranurag1999@gmail.com>
2023-07-06 14:23:50 +0530

    Added two more functions.

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,3 +1,4 @@
+from werkzeug.utils import secure_filename
 from flask import render_template, redirect, url_for
 from flask_mail import Message
 from accounts.extensions import mail
@@ -11,8 +12,15 @@ def unique_uid():
 def unique_security_token(token_len=48):
     return secrets.token_hex(token_len)
 
-def page_not_found():
-    return render_template('error.html')
+def get_unique_filename(filename=None):
+    if not filename:
+        return None
+    filename = secure_filename(filename).split(".")
+    return "{}.{}".format(str(uuid.uuid4()), filename[len(filename)-1])
+
+def remove_existing_file(path=None):
+    if os.path.isfile(path=path):
+        os.remove(path)
 
 def send_mail(subject, recipients, body):
     sender = os.environ.get('MAIL_USERNAME', None)
