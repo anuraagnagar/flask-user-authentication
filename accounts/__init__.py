994a408548fda1794af1b40e45a2ba2a538aa408
Anurag Nagar <nagaranurag1999@gmail.com>
2023-07-21 09:33:53 +0530

    Updated __init__.py

--- accounts/__init__.py
+++ accounts/__init__.py
@@ -42,7 +42,7 @@ def config_application(app):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # Flask-Mail configuration
-    app.config['MAIL_SERVER'] = ''
+    app.config['MAIL_SERVER'] = 'your_mail_server'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = 'your_email@example.com'
