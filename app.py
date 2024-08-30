882470f168508f64c0f30de585cb756bf8d61f8d
Anurag Nagar <nagaranurag1999@gmail.com>
2024-08-30 23:26:09 +0530

    added: config type for application settings.

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import os
+
 from accounts import create_app
 
-app = create_app()
+config_type = os.getenv("FLASK_ENV")
+
+app = create_app(config_type)
 
 if __name__ == "__main__":
     app.run()
